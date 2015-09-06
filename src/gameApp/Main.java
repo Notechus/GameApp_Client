c63d37830d233dbe998141efe5ed406e34e2bdea
Sebastian Paulus <seba0713@gmail.com>
2015-09-06 18:08:44 +0200

    was fucking around with gameApp package(created new repo)

---
+++ src/gameApp/Main.java
@@ -0,0 +1,14 @@
+package gameApp;
+
+/**
+ *
+ * @author Notechus
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        Game game = new Game();
+        game.run();
+    }
+
+}
